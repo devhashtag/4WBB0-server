2bf4124f9bdf45c441d459f554092ae37e475d6d
Heijstek <p.j.heijstek@student.tue.nl>
2020-09-25 13:20:26 +0200

    Basic echo websocket server

--- PocketServer/Startup.cs
+++ PocketServer/Startup.cs
@@ -7,18 +7,21 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Net.Mime;
+using System.Text;
 
 namespace PocketServer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -28,13 +31,41 @@ namespace PocketServer
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+
+            // The configured values are the default values.
+            app.UseWebSockets(new WebSocketOptions()
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 4 * 1024
             });
+
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("Request!");
+
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    await Echo(context, webSocket);
+                }
+            });
+
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!result.CloseStatus.HasValue)
+            {
+                Console.WriteLine("Received a message: " + Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
